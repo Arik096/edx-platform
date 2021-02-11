d70351e9a9eec535cb717ce3cab110170499e896
SaadYousaf <saadyousaf@A006-00314.local>
2021-02-11 13:52:40 +0500

    quality fix

--- lms/djangoapps/discussion/rest_api/utils.py
+++ lms/djangoapps/discussion/rest_api/utils.py
@@ -4,6 +4,7 @@ Utils for discussion API.
 
 from lms.djangoapps.discussion.django_comment_client.utils import has_discussion_privileges
 
+
 def discussion_closed_for_user(course, user):
     """
     Check if course discussion are closed or not for user.
